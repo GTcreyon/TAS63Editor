4e56a5bdae627b1d4e6a5478aa4fceba34911d16
GTcreyon <plaidyoshi@gmail.com>
2021-06-20 09:22:49 +0100

    Compression groundwork

--- DataManager/DataManager.cs
+++ DataManager/DataManager.cs
@@ -23,6 +23,21 @@ namespace DataManagerCore
 			return content.Substring(36, content.Length-1 - 36);
 		}
 
+		private static IReadOnlyList<string> CompressData(List<string> data)
+		{
+			var multiplier = 0;
+					multiplier = 0;
+				}
+			}
+			return data;
+		}
+
+		private static IReadOnlyList<string> CompressMouse(List<string> mouse)
+		{
+			CompressData(mouse); //Somewhat inefficient, but much cleaner
+			return mouse.Select(x => x.Remove(1, 1)).ToList(); //Remove first comma, unneeded
+		}
+
 		public static void SaveTxt(string path, List<string> keys, List<string> mouse, List<string> rng)
 		{
 			File.WriteAllText(path, string.Join("#", keys) + "/" + string.Join("#", mouse) + "/" + string.Join("#", rng));
